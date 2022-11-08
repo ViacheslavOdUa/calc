d027f3957c8a5b3981a9d0b02e3e4402b1bfdc0f
ViacheslavOdUa <1aosikup1@gmail.com>
2022-11-09 01:18:15 +0200

    .

---
+++ js/cals.js
@@ -0,0 +1,95 @@
+let textOut = document.querySelector('.result');
+let clearA = document.querySelector('.deleteAll');
+let clearL = document.querySelector('.deleteLast');
+let submit = document.querySelector('.submit')
+let color = document.querySelector('.color')
+let calc = document.querySelector('.calc')
+function col(){
+    calc.classList.toggle('col');
+}
+textOut.textContent = 0;
+let firstValue = ''
+let secondValue = ''
+let operator = null;
+let j = false;
+
+let keyBtn = document.querySelector('.calc_input');
+keyBtn.onclick =(event)=>{
+if(event.target.classList.contains('number') && j == false) {
+const key = event.target.textContent;
+firstValue+=key;
+textOut.textContent=firstValue}
+else if(event.target.classList.contains('operator')){
+    const oper = event.target.textContent;
+    operator = oper;
+    textOut.textContent=firstValue + operator;
+    j = true;
+    secondValue = ''
+    console.log(firstValue, operator, secondValue)
+}else if(event.target.classList.contains('number') && j !== false){
+    const num = event.target.textContent;
+    secondValue+=num;
+    textOut.textContent=firstValue + operator + secondValue;
+    console.log(firstValue, operator, secondValue)
+}
+}
+function subm(){
+    let answer = 0;
+    switch(operator){
+        case '+':
+           answer = +firstValue + +secondValue;
+           textOut.textContent = answer
+            break;
+        case '-':
+           answer = +firstValue - +secondValue;
+           textOut.textContent = answer
+            break; 
+        case '*':
+           answer = +firstValue * +secondValue;
+           textOut.textContent = answer
+            break; 
+        case '/':
+           answer = +firstValue / +secondValue;
+           textOut.textContent = answer
+            break;      
+    }
+}
+
+function clearLast(){
+    let content = textOut.textContent;
+    if (content == 0) return;
+    else if(content.length == 1){
+        textOut.textContent = '0'
+        firstValue = ''
+    } else if(secondValue == "" && operator ==''){
+        content = content.slice(0, -1);
+        textOut.textContent = content
+        firstValue = firstValue.slice(0, -1);
+        j = false;
+        console.log(firstValue, operator, secondValue)
+    }
+    else if(secondValue == "" && operator !== ''){
+      operator = '';
+      content = content.slice(0, -1);
+      textOut.textContent = content
+    }
+    else{
+        content = content.slice(0, -1);
+        textOut.textContent = content
+        secondValue=secondValue.slice(0, -1);
+       
+    }
+}
+function clearAll(){
+    firstValue = '';
+    let operator = null;
+    let secondValue = '';
+    j = false;
+    num = ''
+    textOut.textContent = '0';
+    console.log(operator, secondValue, firstValue, j)
+}
+clearA.onclick = clearAll;
+clearL.onclick = clearLast;
+submit.onclick = subm;
+color.onclick = col;
